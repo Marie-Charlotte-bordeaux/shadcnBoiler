b0115d4fef774a4b240d5a147adba171d35fd8f3
Alapatate <neilamari20@gmail.com>
2026-01-27 10:12:19 +0100

    UPDATE : product services , and product admin page

--- src/pages/admin/products.jsx
+++ src/pages/admin/products.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { productsServices } from '@/lib/services/productsServices'
 
 export const ProductsAdminPage = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    productsServices.getAllProducts().then(data => setProducts(data));
+  }, []);
+
+  console.log(products);
+
   return (
     <div>Products</div>
   )
